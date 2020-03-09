9bef47e7b5a4f0083e295a4ea2f06a47b418fc1f
Diego Collares <diego.collares@gmail.com>
2020-03-09 01:49:52 -0300

    chagned icons android

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '5• Igreja Batista de Taubaté',
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -4,8 +4,28 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+          backgroundColor: Color(0xffcecece),
+          title: Text("VIBT"),
+          centerTitle: true,
+          elevation: 0),
       body: SafeArea(
         child: Text("Hello Igreja!"),
+      ),
+      drawerEdgeDragWidth: 0,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
