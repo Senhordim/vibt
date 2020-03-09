cadd58370832429908c85794b8f7cd56762a1180
Diego Collares <diego.collares@gmail.com>
2020-03-09 00:37:13 -0300

    create minimal structure

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:vibt/pages/home/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '5• Igreja Batista de Taubaté',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
